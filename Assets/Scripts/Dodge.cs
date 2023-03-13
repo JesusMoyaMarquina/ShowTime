10bd5b6c33152031e6c4c27910b37e78f9abf9c2
Sergio <sergiolopezgarcia9@gmail.com>
2023-03-13 20:25:24 +0100

    Primeros Pasos Dodge

--- Assets/Scripts/Dodge.cs
+++ Assets/Scripts/Dodge.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Dodge : MonoBehaviour
 {
+    public float JumpForce;
+
+    private Rigidbody2D Rigidbody2D;
+    private Animator Animator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Rigidbody2D = GetComponent<Rigidbody2D>();
+        Animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space)) DoDodge();
+    }
+
+    private void DoDodge()
+    {
+        Animator.SetBool("Dodge", true);
+        Rigidbody2D.AddForce(Vector2.up * JumpForce);
     }
 }
