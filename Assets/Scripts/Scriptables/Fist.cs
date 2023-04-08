947f6a7ca7e68b5a92840da96e5011c837fecb73
Alejandro López Poblete <alexlp0140@gmail.com>
2023-04-08 13:22:41 +0200

    Ataque finalizado

--- Assets/Scripts/Scriptables/Fist.cs
+++ Assets/Scripts/Scriptables/Fist.cs
@@ -4,23 +4,45 @@ using UnityEngine;
 
 public class Fist : Weapon
 {
+    protected Vector2[] atkDist = new Vector2[2];
 
+    protected GameObject enemyClose = null;
+
+    protected float[] tiempoCD = new float[2];
+    protected float[] damageDeal = new float[2];
+
+    protected float atkMng = 10;
 
     void Start()
     {
-        
+        atkDist[0] = new Vector2();
+        atkDist[1] = new Vector2();
+
+        tiempoCD[0] = 1;
+        tiempoCD[1] = 1.5f;
+
+        damageDeal[0] = 15;
+        damageDeal[1] = 25;
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            //Putaso flojo
+            //Golpe flojo
+
+            if (enemyClose != null)
+                AttackB(atkDist[0], enemyClose, tiempoCD[0], atkMng, damageDeal[0]);
+
         }
 
         if (Input.GetKeyDown(KeyCode.Alpha2))
         {
             //Putaso al suelo
+
+            if (enemyClose != null)
+                AttackB(atkDist[1], enemyClose, tiempoCD[1], atkMng, damageDeal[1]);
+
         }
     }
 }
