06e2bbec6b58ea724997c7a3ea1d3a99f2e06eed
Moya <moyamarquinajesus@gmail.com>
2023-05-14 00:17:51 +0200

    Fix enemigos cuerpo a cuerpo no atacaban

--- Assets/Scripts/Units/DistanceEM.cs
+++ Assets/Scripts/Units/DistanceEM.cs
@@ -26,10 +26,12 @@ public class DistanceEM : EnemyMovement
         SetAnimation();
     }
 
-    public override void Attacking()
+    public override void Attacking() { }
+
+    public void ThrowAttack()
     {
         arrow = GetComponent<ObjectPool>().GetPooledObject();
-        
+
         if (arrow != null)
         {
             arrow.transform.SetPositionAndRotation(new Vector3(transform.position.x, transform.position.y, 0), Quaternion.identity);
